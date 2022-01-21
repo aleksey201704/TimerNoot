80d541b02a739459f9857eeb4a594c3ec5d806cd
Aleksey <sav459811@mail.ru>
2022-01-21 13:54:31 +0500

    Add project files.

---
+++ TimerNoot/MyForm.cpp
@@ -0,0 +1,16 @@
+#include "MyForm.h"
+#include <Windows.h>
+
+using namespace TimerNoot;
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+//[STAThread]
+void main(/*array<String^>^ arg*/) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    
+    TimerNoot::MyForm form; //WinFormsTest - имя вашего проекта
+    Application::Run(% form);
+}
---
+++ TimerNoot/MyForm.h
@@ -0,0 +1,154 @@
+#pragma once
+#include<iostream>
+#include <stdlib.h>
+
+namespace TimerNoot {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Timer^ timer1;
+	protected:
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Timer^ timer2;
+	private: System::ComponentModel::IContainer^ components;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->timer2 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->SuspendLayout();
+			// 
+			// timer1
+			// 
+			this->timer1->Interval = 500;
+			this->timer1->Tick += gcnew System::EventHandler(this, &MyForm::timer1_Tick);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(348, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(152, 45);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Exit";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(272, 19);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(70, 25);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"label1";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(196, 19);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(70, 25);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"label2";
+			// 
+			// timer2
+			// 
+			this->timer2->Enabled = true;
+			this->timer2->Interval = 1000;
+			this->timer2->Tick += gcnew System::EventHandler(this, &MyForm::timer2_Tick);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::Info;
+			this->ClientSize = System::Drawing::Size(512, 422);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	int TimerCounterS;
+	int TimerCounterM;
+	
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		exit(0);
+	}
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		TimerCounterS = 0;
+		timer1->Enabled = true;
+		timer1->Interval = 1000;
+
+		label1->Text = timer1->ToString();
+	}
+	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+		
+		TimerCounterS++;
+		label1->Text = "0";
+		label1->Text = TimerCounterS.ToString();
+	}
+	private: System::Void timer2_Tick(System::Object^ sender, System::EventArgs^ e) {
+		TimerCounterM = 0;
+		label2->Text = "00";
+
+	}
+};
+}
